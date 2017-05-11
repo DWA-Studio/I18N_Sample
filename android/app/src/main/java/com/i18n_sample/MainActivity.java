e1757e7a97c674c91bf294b2ab016e3790ea3ea4
Tybi <developer@dwastudio.fr>
2017-05-11 07:39:18 +0200

    add project sources &&  react-native-i18n

---
+++ android/app/src/main/java/com/i18n_sample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.i18n_sample;
+
+import com.facebook.react.ReactActivity;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "I18N_Sample";
+    }
+}
